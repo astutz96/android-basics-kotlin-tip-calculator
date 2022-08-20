00c0142868d14635fa74228a184b4530ddf88111
austinstutzman <alsghs49@gmail.com>
2022-08-20 10:49:51 -0400

    fixing bug where tipResult wasn't updating to refleft no value entered in the cost_of_service

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -20,7 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull() ?: return
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null){
+            binding.tipResult.text = ""
+            return
+        }
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
